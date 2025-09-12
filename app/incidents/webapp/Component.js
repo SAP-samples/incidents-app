ce7ba2fe00c8613cfa39714480ffc5d9c6daaff5
i329347 <gopal.anand@sap.com>
2025-09-12 10:44:57 +0530

    kyma base code

--- app/incidents/webapp/Component.js
+++ app/incidents/webapp/Component.js
@@ -3,7 +3,7 @@ sap.ui.define(
     function (Component) {
         "use strict";
 
-        return Component.extend("ns.incidents.Component", {
+        return Component.extend("ns.incidents33.Component", {
             metadata: {
                 manifest: "json"
             }
